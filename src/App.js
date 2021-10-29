d2506ecbad5e6c35fd613a764b37c5eb2ed40b92
Cedric Hazoume <cedric.hazoume@webbfontaine.com>
2021-10-29 17:03:59 +0100

    setting of RestaurantsList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Restaurant from './components/Restaurant'
+import RestaurantsList from './components/RestaurantsList'
+import AddReview from './components/AddReview'
+import Login from './components/Login'
+
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  const login = async (user) => {
+    setUser(user)
+  }
+
+  const logout = async () => {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/restaurants" className="navbar-brand">
+            Restaurant Reviews
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/restaurants"} className="nav-link">
+                Restaurants
+              </Link>
+            </li>
+            <li className="nav-item" >
+              { user ? (
+                <a onClick={logout} className="nav-link" style={{cursor:'pointer'}}>
+                  Logout {user.name}
+                </a>
+              ) : (            
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+              )}
+
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/restaurants"]} component={RestaurantsList} />
+            <Route 
+              path="/restaurants/:id/review"
+              render={(props) => (
+                <AddReview {...props} user={user} />
+              )}
+            />
+            <Route 
+              path="/restaurants/:id"
+              render={(props) => (
+                <Restaurant {...props} user={user} />
+              )}
+            />
+            <Route 
+              path="/login"
+              render={(props) => (
+                <Login {...props} login={login} />
+              )}
+            />
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 }
